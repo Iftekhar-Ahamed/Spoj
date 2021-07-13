56a2544e278e005271ca6ea2d464224f5eaa22c6
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2021-07-13 14:18:36 +0600

    Create CODER FIRST PROBLEM.cpp

---
+++ CODER FIRST PROBLEM.cpp
@@ -0,0 +1,62 @@
+/***
+**      Bismillahir Rahmanir Rahim
+**              ALLAHU AKBAR
+**
+**     Author: Iftekhar Ahamed Siddiquee
+**     Bangladesh University of Business and Technology,
+**     Dept. of CSE.
+***/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+using namespace std;
+
+#define read freopen("input.txt","r",stdin)
+#define write freopen("output.txt","w",stdout)
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int uint64;
+#define lld long long int
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+const int mXs = 10000000;
+const double pi = acos(-1.0);
+
+lld solve(lld x){
+    if(x<=2)return x;
+    else if(x%2==0)return 2+solve(x/2);
+    else return 0;
+}
+int main()
+{
+    cin.tie(NULL);
+    //read;
+    //write;
+    ios_base::sync_with_stdio(false);
+    lld x;
+    cin>>x;
+    cout<<solve(x)<<endl;
+    return 0;
+}
